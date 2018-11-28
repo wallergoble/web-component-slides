4f24aeade0c068ea392b2b1a517690cf46bc9f19
Waller Goble <william_w_goble@homedepot.com>
2018-11-28 18:19:10 -0500

    Add theme

---
+++ theme.js
@@ -0,0 +1,11 @@
+import { future } from 'mdx-deck/themes'
+import hopscotch from 'react-syntax-highlighter/styles/prism/hopscotch'
+
+export default {
+    ...future,
+    prism: {
+        style: hopscotch,
+        languages: {
+        }
+    }
+}
