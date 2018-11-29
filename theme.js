df95b142e76ac54970bac01d20f06fa5e36f85a5
Waller Goble <william_w_goble@homedepot.com>
2018-11-28 23:58:51 -0500

    Use right version of highlighter

--- theme.js
+++ theme.js
@@ -1,11 +1,9 @@
-import { future } from 'mdx-deck/themes'
 import hopscotch from 'react-syntax-highlighter/styles/prism/hopscotch'
+import { future } from 'mdx-deck/themes'
 
 export default {
     ...future,
     prism: {
         style: hopscotch,
-        languages: {
-        }
     }
 }
